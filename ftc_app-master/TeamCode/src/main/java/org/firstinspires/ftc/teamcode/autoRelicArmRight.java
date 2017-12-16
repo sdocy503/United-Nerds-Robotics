d4e1b32695c4b76252e8df9d678be8757e826d67
sdocy503 <docystephen@gmail.com>
2017-12-16 10:41:48 -0800

    Added second autonomous

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoRelicArmRight.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoRelicArmRight.java
@@ -55,8 +55,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="AutoRelicArm", group="Iterative Opmode")
-public class autoRelicArm extends LinearOpMode {
+@Autonomous(name="AutoRelicArmRight", group="Iterative Opmode")
+public class autoRelicArmRight extends LinearOpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
